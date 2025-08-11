6557085f082fd84ce7136bd6b7c73fc153c7b8c6
Ruslan <1437dada@mail.ru>
2025-08-11 16:30:35 +0300

    add 3 homework

--- hw03-annotations/src/main/java/ru/rasulov/App.java
+++ hw03-annotations/src/main/java/ru/rasulov/App.java
@@ -1,58 +1,7 @@
 package ru.rasulov;
 
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
-
 public class App {
-
-    public static void main(String[] args) throws ClassNotFoundException {
-        TestService testHandler = new TestService();
-        int totalTests = 0;
-        int failedTests = 0;
-
-        Map<TestService.MethodType, List<Method>> testMethods = testHandler.handleTests(FakeTest.class.getName());
-        List<Method> tests = testMethods.get(TestService.MethodType.TEST);
-        totalTests = tests.size();
-
-        for (Method testMethod : tests) {
-            boolean testFailed = false;
-            FakeTest testInstance = new FakeTest();
-
-            for (Method beforeMethod : testMethods.get(TestService.MethodType.BEFORE)) {
-                try {
-                    beforeMethod.invoke(testInstance);
-                } catch (Exception e) {
-                    throw new RuntimeException("Before Block crush", e);
-                }
-            }
-
-                try {
-                    testMethod.invoke(testInstance);
-                } catch (Exception e) {
-                    testFailed = true;
-
-                }
-
-
-            for (Method afterMethod : testMethods.get(TestService.MethodType.AFTER)) {
-                try {
-                    afterMethod.invoke(testInstance);
-                } catch (Exception e) {
-                    throw new RuntimeException("After Block crush", e);
-                }
-            }
-
-            if (testFailed) {
-                failedTests++;
-            }
-        }
-
-        int successfulTests = totalTests - failedTests;
-
-        System.out.println("-----------------------");
-        System.out.println("Total tests: " + totalTests);
-        System.out.println("Failed tests: " + failedTests);
-        System.out.println("Successful tests: " + successfulTests);
+    public static void main(String[] args) throws Exception {
+        new TestRunner().runTests(FakeTest.class.getName());
     }
 }
